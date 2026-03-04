7a53928f4a1b27bd12a324f4f835b3c0e6ba9d91
kanishka <98266873+Kanishka1256@users.noreply.github.com>
2026-03-04 22:48:04 +0530

    Update contents.js

--- contents.js
+++ contents.js
@@ -2,4 +2,4 @@ document.addEventListener('copy', e => e.stopImmediatePropagation(), true);
 document.addEventListener('paste', e => e.stopImmediatePropagation(), true);
 document.addEventListener('cut', e => e.stopImmediatePropagation(), true);
 
-alert("Copy/Paste restriction bypass attempt done. Try now.");
+;
